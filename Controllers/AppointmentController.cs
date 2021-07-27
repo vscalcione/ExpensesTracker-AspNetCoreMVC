a3ded03b56c2dda602a5efaaf4c4d9c97691487f
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-27 12:07:14 +0200

    Initial commit

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = " + id);
+        }
+    }
+}
